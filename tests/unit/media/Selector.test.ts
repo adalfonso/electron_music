013549f3532512268e10cad89219216ee5af5305
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2021-03-20 21:33:50 -0400

    Improve code coverage in Selector

--- tests/unit/media/Selector.test.ts
+++ tests/unit/media/Selector.test.ts
@@ -103,15 +103,59 @@ describe("media/Selector", () => {
         category: "genre",
         expected: [
           {
-            path: "boz.flac",
-            artist: "Bozzy",
-            album: "Booze",
-            duration: "5:33",
-            genre: "BotBoz",
-            title: "Bonzon",
+            path: "baz4.flac",
+            artist: "Bazzler",
+            album: "",
+            duration: "5:34",
+            genre: "Baz Baz",
+            title: "Bazlishli",
             track: "1",
-            year: "2021",
-            file_type: "M4A"
+            year: "2000",
+            file_type: "FLAC"
+          },
+          {
+            path: "baz.flac",
+            artist: "Bazzler",
+            album: "Baz",
+            duration: "18:46",
+            genre: "Baz Baz",
+            title: "Bazly",
+            track: "1",
+            year: "2019",
+            file_type: "FLAC"
+          },
+          {
+            path: "baz1.flac",
+            artist: "Bazzler",
+            album: "Baz",
+            duration: "2:14",
+            genre: "Baz Baz",
+            title: "Bazly",
+            track: "2",
+            year: "2019",
+            file_type: "FLAC"
+          },
+          {
+            path: "baz2.flac",
+            artist: "Bazzler",
+            album: "The Compilation",
+            duration: "5:34",
+            genre: "Baz Baz",
+            title: "Bazlish",
+            track: "1",
+            year: "2011",
+            file_type: "FLAC"
+          },
+          {
+            path: "baz3.flac",
+            artist: "Bazzler",
+            album: "The Compilation",
+            duration: "3:34",
+            genre: "Baz Baz",
+            title: "This is a different compilation",
+            track: "1",
+            year: "2001",
+            file_type: "FLAC"
           }
         ]
       }
